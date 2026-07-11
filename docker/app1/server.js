6c3c3c623fdcb100699dc98f9e6358ca799bf84d
bani1610 <sholahuddinrq6@gmail.com>
2026-07-11 19:16:53 +0000

    aplikasi

---
+++ docker/app1/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Halo dari App1! Waktu server: ' + new Date().toISOString());
+});
+
+app.listen(PORT, () => {
+  console.log(`App1 running on port ${PORT}`);
+});
